7880232c6b392d8328b54152c8329d95cb605194
Landon Mann <mann.landon@gmail.com>
2018-10-20 11:33:13 +0900

    Added editable text box with simple unit tests for mode switching

---
+++ src/components/EditableTextBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="editMode">
+     <input type="text" v-if="editMode" placeholder="value"/>
+  </div>
+  <div v-else>
+    <h1>{{ value }}</h1>
+  </div>
+   
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() public value!: string;
+  @Prop({default: false}) public editMode!: boolean;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ tests/unit/editabletextbox.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import EditableTextBox from '@/components/EditableTextBox.vue';
+
+describe('EditableTextBox.vue', () => {
+  it('starts off as a regular text or header', () => {
+    const value = 'Test';
+    const wrapper = shallowMount(EditableTextBox, {
+      propsData: { value },
+    });
+    expect(wrapper.text()).to.include(value);
+    expect(wrapper.contains('input')).to.equals(false);
+  });
+
+  it('switches to input in editMode', () => {
+    const value = 'Test';
+    const wrapper = shallowMount(EditableTextBox, {
+      propsData: { value, editMode: true },
+    });
+    expect(wrapper.contains('input')).to.equals(true);
+  });
+});
